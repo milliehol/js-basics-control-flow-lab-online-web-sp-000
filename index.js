bb0d1410ba8bf42a154fbd988d03d2615dc3b774
Carla Holcomb <carla.programmer@gmail.com>
2019-09-01 15:23:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function scuberGreetingForFeet(){
-  if (someNumber < 400)
+  if (someNumber <= 400)
 
   result = "This ride is for free"
 
   elseif (someNumber >= 400)
 
-  result = "This ride is for free"
+  result = "This ride is not free"
+
+  elseif (someNumber > 2000)
+
+  result = "This ride costs $30"
+
+  elseif (someNumber > 2500)
+
+  result = "This ride is not allowed"
 
 
 
